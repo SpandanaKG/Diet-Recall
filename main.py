54560c81a086069308468753e834ae0e0a218687
SpandanaKG <154040257+SpandanaKG@users.noreply.github.com>
2024-04-11 21:15:17 +0530

    Update main.py

--- main.py
+++ main.py
@@ -13,8 +13,6 @@ import login
 
 Window.size = (370, 697)
 
-
-
 KV = """
 
 ScreenManager:
@@ -25,7 +23,7 @@ ScreenManager:
     SettingsScreen:
     LogoutScreen:
     LoginScreen:
- 
+
 <MenuScreen>:
     name:'menu'
     Screen:
@@ -50,7 +48,7 @@ ScreenManager:
                     orientation: 'vertical'
                     padding: '8dp'
                     spacing: '20dp'
-                    
+
                     ScrollView:
                         MDList:
                             OneLineIconListItem:
@@ -122,7 +120,7 @@ ScreenManager:
                     orientation: 'vertical'
                     padding: '8dp'
                     spacing: '20dp'
-                    
+
                     ScrollView:
                         MDList:
                             OneLineIconListItem:
@@ -167,7 +165,7 @@ ScreenManager:
                                     on_release: 
                                         nav_drawer.set_state('close')
                                         app.root.current='logout'    
-                                    
+
 <ChallengesScreen>:
     name:'challenges'
     Screen:
@@ -188,14 +186,14 @@ ScreenManager:
                                 text:"Database to be designed"
                                 halign: 'center'
                                 pos_hint: {'center_x': .52, 'y': .7}
-                                
+
             MDNavigationDrawer:
                 id: nav_drawer
                 BoxLayout:
                     orientation: 'vertical'
                     padding: '8dp'
                     spacing: '20dp'
-                    
+
                     ScrollView:
                         MDList:
                             OneLineIconListItem:
@@ -267,7 +265,7 @@ ScreenManager:
                     orientation: 'vertical'
                     padding: '8dp'
                     spacing: '20dp'
-                    
+
                     ScrollView:
                         MDList:
                             OneLineIconListItem:
@@ -367,8 +365,8 @@ ScreenManager:
                         MDScreen:
                             Log:
                                 height:'75dp'
-                                
-                
+
+
 <LoginScreen>:
     name:'login'
     Screen:
@@ -379,7 +377,7 @@ ScreenManager:
                     MDLabel:
                         text:'HelloWorld'
                         halign: 'center'
-                                                                                                  
+
 """
 
 username_helper = """
@@ -442,44 +440,61 @@ MDScreen:
             app.show_popup()
 """
 
+
 class MenuScreen(Screen):
     pass
+
 
 class ProfileScreen(Screen):
     pass
 
+
 class ChallengesScreen(Screen):
     pass
+
 
 class FeedbackScreen(Screen):
     pass
 
+
 class SettingsScreen(Screen):
     pass
 
+
 class LogoutScreen(Screen):
     pass
+
 
 class LoginScreen(Screen):
     pass
 
 
 class Menu(Screen):
-    def __init__(self,**kwargs):
+    def __init__(self, **kwargs):
         super(Menu, self).__init__(**kwargs)
-        eat = MDRectangleFlatButton(text="Eaten\n\n____kcal", size_hint=(.15,.31), pos_hint={'center_x': 0.21, 'y': 1.55})
-        eat1 = MDRectangleFlatButton(text="Calories\n\n Remaining\n\n____kcal", size_hint=(.15, .31), pos_hint={'center_x': 0.79, 'y': 1.55})
-        btn_flat = MDRectangleFlatButton(text="BreakFast\n___Recommended cal\n___cal consumed ", halign="left", pos_hint={'center_x' : 0.5,'y': 1.25}, size_hint=(.78, 0),on_release = self.on_button_press)
+        eat = MDRectangleFlatButton(text="Eaten\n\n____kcal", size_hint=(.15, .31),
+                                    pos_hint={'center_x': 0.21, 'y': 1.55})
+        eat1 = MDRectangleFlatButton(text="Calories\n\n Remaining\n\n____kcal", size_hint=(.15, .31),
+                                     pos_hint={'center_x': 0.79, 'y': 1.55})
+        btn_flat = MDRectangleFlatButton(text="BreakFast\n___Recommended cal\n___cal consumed ", halign="left",
+                                         pos_hint={'center_x': 0.5, 'y': 1.25}, size_hint=(.78, 0),
+                                         on_release=self.on_button_press)
         btn_icon = MDIconButton(icon="plus", halign="right", pos_hint={'center_x': .81, 'y': 1.27})
-        btn_flat1 = MDRectangleFlatButton(text="Lunch\n___Recommended cal\n___cal consumed ", halign="left", pos_hint={'center_x' : 0.5, 'y': 1}, size_hint=(.78, 0))
-        btn_icon2 = MDIconButton(icon="plus",halign="right",pos_hint={'center_x':.81,'y':1.02})
-        btn_flat2 = MDRectangleFlatButton(text="Snacks\n___Recommended cal\n___cal consumed ",halign="left", pos_hint={'center_x' : 0.5, 'y': .75}, size_hint=(.78, 0))
-        btn_icon3 = MDIconButton(icon="plus",halign="right",pos_hint={'center_x':.81,'y':0.77})
-        btn_flat3 = MDRectangleFlatButton(text="Dinner\n___Recommended cal\n___cal consumed ",halign="left", pos_hint={'center_x' : 0.5, 'y': 0.5}, size_hint=(.78, 0))
-        btn_icon4 = MDIconButton(icon="plus",halign="right",pos_hint={'center_x':.81,'y':0.52})
-        btn1 = MDFlatButton(text="Favorites", pos_hint={'center_x': .2, 'center_y': 0.30},md_bg_color="#00e77a", on_release = self.on_button_press)
-        btn2 = MDFlatButton(text="Water Tanker", pos_hint={'center_x': .5, 'center_y': 0.30}, md_bg_color="#00e77a", on_release=self.on_button_press1)
-        btn3 = MDFlatButton(text="Meal Planner", pos_hint={'center_x': .8, 'center_y': 0.30},md_bg_color="#00e77a", on_release = self.on_button_press2)
+        btn_flat1 = MDRectangleFlatButton(text="Lunch\n___Recommended cal\n___cal consumed ", halign="left",
+                                          pos_hint={'center_x': 0.5, 'y': 1}, size_hint=(.78, 0))
+        btn_icon2 = MDIconButton(icon="plus", halign="right", pos_hint={'center_x': .81, 'y': 1.02})
+        btn_flat2 = MDRectangleFlatButton(text="Snacks\n___Recommended cal\n___cal consumed ", halign="left",
+                                          pos_hint={'center_x': 0.5, 'y': .75}, size_hint=(.78, 0))
+        btn_icon3 = MDIconButton(icon="plus", halign="right", pos_hint={'center_x': .81, 'y': 0.77})
+        btn_flat3 = MDRectangleFlatButton(text="Dinner\n___Recommended cal\n___cal consumed ", halign="left",
+                                          pos_hint={'center_x': 0.5, 'y': 0.5}, size_hint=(.78, 0))
+        btn_icon4 = MDIconButton(icon="plus", halign="right", pos_hint={'center_x': .81, 'y': 0.52})
+        btn1 = MDFlatButton(text="Favorites", pos_hint={'center_x': .2, 'center_y': 0.30}, md_bg_color="#00e77a",
+                            on_release=self.on_button_press)
+        btn2 = MDFlatButton(text="Water Tanker", pos_hint={'center_x': .5, 'center_y': 0.30}, md_bg_color="#00e77a",
+                            on_release=self.on_button_press1)
+        btn3 = MDFlatButton(text="Meal Planner", pos_hint={'center_x': .8, 'center_y': 0.30}, md_bg_color="#00e77a",
+                            on_release=self.on_button_press2)
         self.add_widget(eat)
         self.add_widget(eat1)
         self.add_widget(btn_flat)
@@ -493,28 +508,32 @@ class Menu(Screen):
         self.add_widget(btn1)
         self.add_widget(btn2)
         self.add_widget(btn3)
-    def on_button_press(self,instance):
+
+    def on_button_press(self, instance):
         # app = MDApp.get_running_app()
         # app.root.current = brekie()
         print("BTS!!!!")
 
-
-    def on_button_press1(self,instance):
+    def on_button_press1(self, instance):
         # app = MDApp.get_running_app()
         # app.root.current = 'menu'
         print("The Love")
 
-    def on_button_press2(self,instance):
+    def on_button_press2(self, instance):
         # app = MDApp.get_running_app()
         # app.root.current = 'menu'
         print("The Favourites!!")
+
+
 class Profile(Screen):
     def __init__(self, **kwargs):
         super(Profile, self).__init__(**kwargs)
-        img=Image(source='profile1.jpg',size_hint=(.15,.31),pos_hint={'center_x':0.51,'y':1.55})
+        img = Image(source='profile1.jpg', size_hint=(.15, .31), pos_hint={'center_x': 0.51, 'y': 1.55})
         self.username = Builder.load_string(username_helper)
         self.add_widget(self.username)
         self.add_widget(img)
+
+
 class Feed(Screen):
     def _init_(self, **kwargs):
         super(Feed, self)._init_(**kwargs)
@@ -525,29 +544,33 @@ class Feed(Screen):
         user_feed = self.screen_text
         global feed_obj
         feed_obj = self
-        # self.screen_text = ''
+
 
 
 class Log(Screen):
     def __init__(self, **kwargs):
         super(Log, self).__init__(**kwargs)
-        label = MDFlatButton(text="Wish to leave???",pos_hint={'center_x': .5, 'center_y': 1.5},font_style='H4')
-        button=MDRectangleFlatButton(text="Logout",pos_hint={'center_x':.5,'center_y':1},on_release=self.show)
+        label = MDFlatButton(text="Wish to leave???", pos_hint={'center_x': .5, 'center_y': 1.5}, font_style='H4')
+        button = MDRectangleFlatButton(text="Logout", pos_hint={'center_x': .5, 'center_y': 1}, on_release=self.show)
         self.add_widget(button)
         self.add_widget(label)
-    def show(self,obj):
-        close_button= MDFlatButton(text='logout',on_release=self.close_button)
-        no_button= MDFlatButton(text='No',on_release=self.n_button)
-        self.dialog = MDDialog(text='Are u sure u want to logout?',buttons=[close_button,no_button])
+
+    def show(self, obj):
+        close_button = MDFlatButton(text='logout', on_release=self.close_button)
+        no_button = MDFlatButton(text='No', on_release=self.n_button)
+        self.dialog = MDDialog(text='Are u sure u want to logout?', buttons=[close_button, no_button])
         self.dialog.open()
-    def close_button(self,LoginScreen):
+
+    def close_button(self, LoginScreen):
         self.dialog.dismiss()
         app = MDApp.get_running_app()
         app.root.current = 'login'
-    def n_button(self,MenuScreen):
+
+    def n_button(self, MenuScreen):
         self.dialog.dismiss()
         app = MDApp.get_running_app()
         app.root.current = 'menu'
+
 
 class DietRecallApp(MDApp):
     def build(self):
@@ -560,8 +583,8 @@ class DietRecallApp(MDApp):
         screen1 = Screen()
         theme_cls = ThemeManager()
         self.theme_cls.primary_palette = ("Green")
-        self.theme_cls.primary_hue="A700"
-        self.theme_cls.theme_style=("Light")
+        self.theme_cls.primary_hue = "A700"
+        self.theme_cls.theme_style = ("Light")
         return screen
 
     def on_switch_active(self, instance_switch, value):
@@ -569,27 +592,29 @@ class DietRecallApp(MDApp):
             self.theme_cls.theme_style = 'Dark'
         else:
             self.theme_cls.theme_style = 'Light'
+
     def get_screen(self, screen_name):
         if screen_name == "feedback":
             return FeedbackScreen()
         else:
             return None
 
-def show_popup(self):
+
+    def show_popup(self):
         conn = sqlite3.connect('diet_db.db')
         cursor = conn.cursor()
         feedback_text = user_feed.text
-        cursor.execute("insert into feedback values(?)",(feedback_text,))
+        cursor.execute("insert into feedback values(?)", (feedback_text,))
         conn.commit()
         conn.close()
         # self.screen_text = ''
-        
+
         feed_obj.screen_text.text = ''
-        
-        
+
         toast("Feedback Saved!")
         app = MDApp.get_running_app()
-        app.root.current = 'menu'
+        app.root.current='menu'
+
 
     def show_popup1(self):
         # conn = sqlite3.connect('diet_db.db')
@@ -602,8 +627,8 @@ def show_popup(self):
         app.root.current = 'menu'
 
 
-
 def main():
     DietRecallApp().run()
+
 
 main()
